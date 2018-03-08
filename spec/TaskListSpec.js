cc44687880fc8d4fecd9d2487bd246ffcd217df5
Zanidd <ninijay93@gmail.com>
2018-03-08 21:04:03 +0100

    First draft oft TaskListSpec.js

--- spec/TaskListSpec.js
+++ spec/TaskListSpec.js
@@ -1,58 +1,25 @@
-describe("Player", function() {
-  var player;
-  var song;
+describe("TaskList", function() {
+  var tasklist;
 
   beforeEach(function() {
-    player = new Player();
-    song = new Song();
+    tasklist = new TaskList("Tasklist");
   });
 
-  it("should be able to play a Song", function() {
-    player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(song);
-
-    //demonstrates use of custom matcher
-    expect(player).toBePlaying(song);
+  it("should have tasks", function() {
+    tasklist.add(new Task('name1'));
+    tasklist.add(new Task('name2'));
+    expect(tasklist.count()).toEqual(2);
   });
 
-  describe("when song has been paused", function() {
-    beforeEach(function() {
-      player.play(song);
-      player.pause();
-    });
-
-    it("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
-
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
-    });
-
-    it("should be possible to resume", function() {
-      player.resume();
-      expect(player.isPlaying).toBeTruthy();
-      expect(player.currentlyPlayingSong).toEqual(song);
-    });
+  it("should have a title", function(){
+    expect(tasklist.title).toEqual("Tasklist");
   });
 
-  // demonstrates use of spies to intercept and test method calls
-  it("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
-
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
-
-  //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    it("should throw an exception if song is already playing", function() {
-      player.play(song);
-
-      expect(function() {
-        player.resume();
-      }).toThrowError("song is already playing");
-    });
+  it("should be able to remove"), function(){
+    var task = new Task("cooltask");
+    tasklist.add(task);
+    expect(tasklist.count()).toEqual(1);
+    tasklist.remove(task);
+    expect(tasklist.count()).toEqual(0);
   });
 });
