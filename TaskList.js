6a0341e8423d341f8cae79ded7726830a7d10534
Nick <duongnic@students.zhaw.ch>
2018-03-10 14:53:12 +0100

    Min shit

--- TaskList.js
+++ TaskList.js
@@ -1,3 +1,24 @@
 function TaskList(title){
     this.title = title;
+    var tasks = [];
+    
+    function add(task) {
+    	tasks.push(task);
+    }
+    
+    function remove(taksTitle) {
+    	for(var i = 0; i < tasks.length; i++) {
+    		if(tasks[i].title == taskTitle) {
+    			tasks.splice(i, 1);
+    			break;
+    		}
+    	}
+    }
+    
+    function count() {
+    	return tasks.length;
+    }
+    
+    
 }
+
