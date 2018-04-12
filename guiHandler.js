2d6b17dd0904b290a71592a915cead1412ab1f1c
Zanidd <ninijay93@gmail.com>
2018-04-12 22:24:01 +0200

    added editable list

--- guiHandler.js
+++ guiHandler.js
@@ -9,29 +9,42 @@ $(document).ready(function(){
     function addTaskToGui(task, id)
     {
         console.log(window.openTask);
-       $("#list ul").append('<li id="task-'+id+'">'+window.openTask+' '+task.title+'</li>');
+       $("#list ul").append('<li id="task-'+id+'"><span class="checkit">'+window.openTask+'</span> <span contenteditable="true" class="tsk-tit">'+task.title+'</span></li>');
     };
+
+
+    function editTask(id, text)
+    {
+        window.tlist.getById(id).title = text;
+    };
+
+    $('#list ul').keypress(function(e)
+    {
+        var id=$(e.target).parent().attr("id");
+        var search = id.replace("task-", "");
+        var text = $(e.target).text();
+        console.log(search);
+        editTask(search, text);
+    });
 
     function completeTask(id)
     {
         var search = "#"+id;
         $(search).addClass("completed");
-        var text = $(search).text();
-        text = text.replace(window.openTask, "");
-        text = "<i class=\"fa fa-check\"></i> " + text;
-        $(search).html(text);
+        search = search+" .checkit";
+        $(search).html("<i class=\"fa fa-check\"></i>");
     }
 
-    $('#list ul').on("click", "li", function(e) 
+    $('#list ul').on("click", ".checkit", function(e) 
     {
-        var id=e.target.id;
+        var id=$(e.target).parent().parent();
+        id = id.attr("id");
         var search = id.replace("task-", "");
-        
         window.tlist.getById(search).setDone();
         completeTask(id);
     });
 
-    $('#addTaskForm').submit(function(e)
+    $('#addTaskForm form').submit(function(e)
     {
         e.preventDefault();
         var tit = $('#addTaskForm').find('input[name="title"]').val();
